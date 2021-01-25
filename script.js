fffb36c8d22226073391947c9a60a6c5436cd36d
PhantomCoder2 <gudduag23@gmail.com>
2021-01-25 13:46:44 +0530

    e

--- script.js
+++ script.js
@@ -6,15 +6,21 @@ var typed=new Typed(".typing",{
     backSpeed:60,
     cursorChar:'_'
 });
+var typed=new Typed(".typing2",{
+    strings:["Self Learner.","Developer.","Designer."],
+    smartBackspace:true,
+    loop:true,
+    typeSpeed:100,
+    backSpeed:60,
+    cursorChar:'|'
+});
 $(document).ready(function(){
     $(window).scroll(function(){
-        $('navbar').toggleClass('sticky', $(this).scrollTop() > 20)
-        /*if (this.scrollY>20){
-            $(".navbar").css('background','#B22222');
-            $(".navbar").css('padding','10px 0px');
+        if(this.scrollY>20){
+            $('.navbar').addClass('sticky');
         }
         else{
-            $('.else').style.removeProperty('background');
+            $('.navbar').removeClass('sticky');
         }
-    })*/
-})
+    })
+});
